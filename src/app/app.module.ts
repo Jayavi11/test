8492b0deb527da82f32d338e74f9de02c12edeae
Jayavi11 <jayavinayakjayakumar@gmail.com>
2022-07-02 16:05:33 +0530

    neww

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { SearchEmployeeComponent } from './search-employee/search-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    SearchEmployeeComponent,
+
   ],
   imports: [
     BrowserModule,
